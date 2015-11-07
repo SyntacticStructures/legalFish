eee4b6b450292a037dda271f2f7eee39a53afebb
Taylor Lea <tlea105@gmail.com>
2015-11-07 01:35:55 -0800

    another initial

--- UPROPE/ContentViewController.swift
+++ UPROPE/ContentViewController.swift
@@ -1,9 +1,34 @@
 //
-//  ContentViewController.swift
-//  UPROPE
+//  PageItemController.swift
+//  Paging_Swift
 //
-//  Created by Computer on 11/6/15.
-//  Copyright © 2015 Computer. All rights reserved.
+//  Created by Olga Dalton on 26/10/14.
+//  Copyright (c) 2014 swiftiostutorials.com. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PageItemController: UIViewController {
+    
+    @IBOutlet weak var titleTextOutlet: UILabel!
+    // MARK: - Variables
+    var itemIndex: Int = 0
+    var titleText = ""
+//    var imageName: String = "" {
+//        
+//        didSet {
+//            
+//            if let imageView = contentImageView {
+//                imageView.image = UIImage(named: imageName)
+//            }
+//            
+//        }
+//    }
+    
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.titleTextOutlet.text = self.titleText
+    }
+}
--- UPROPE/ViewController.swift
+++ UPROPE/ViewController.swift
@@ -2,24 +2,84 @@
 //  ViewController.swift
 //  UPROPE
 //
-//  Created by Computer on 10/14/15.
+//  Created by Computer on 10/12/15.
 //  Copyright © 2015 Computer. All rights reserved.
 //
 
+//temporarily need to parse through html page to get info. This will not be needed when we json from backend
 import UIKit
+var userInfoLabelCollection: [UILabel]!
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPageViewControllerDataSource {
+    private var pageViewController: UIPageViewController?
+    let titles = ["hello", "next Slide. you win"]
+       // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createPageViewController()
+        setupPageControl()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func createPageViewController() {
+        
+        let pageController = self.storyboard!.instantiateViewControllerWithIdentifier("PageController") as! UIPageViewController
+        pageController.dataSource = self
+        
+        if titles.count > 0 {
+            let firstController = getItemController(0)!
+            var startingViewControllers = [PageItemController]()
+            startingViewControllers.append(firstController)
+            pageController.setViewControllers(startingViewControllers, direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        }
+        
+        pageViewController = pageController
+        addChildViewController(pageViewController!)
+        self.view.addSubview(pageViewController!.view)
+        pageViewController!.didMoveToParentViewController(self)
     }
-
-
+    
+    private func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.grayColor()
+        appearance.currentPageIndicatorTintColor = UIColor.whiteColor()
+        appearance.backgroundColor = UIColor.darkGrayColor()
+    }
+    
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex > 0 {
+            return getItemController(itemController.itemIndex-1)
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex+1 < titles.count {
+            return getItemController(itemController.itemIndex+1)
+        }
+        
+        return nil
+    }
+    
+    private func getItemController(itemIndex: Int) -> PageItemController? {
+        
+        if itemIndex < titles.count {
+            let pageItemController = self.storyboard!.instantiateViewControllerWithIdentifier("ItemController") as! PageItemController
+            pageItemController.itemIndex = itemIndex
+            pageItemController.titleText = titles[itemIndex]
+            return pageItemController
+        }
+        
+        return nil
+    }
 }
 
